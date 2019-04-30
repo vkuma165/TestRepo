f3bbd4fbb96315c92fda9e2a818f77289534129c
vkuma165 <50135683+vkuma165@users.noreply.github.com>
2019-04-30 14:25:19 +0530

    Testing

---
+++ SampleDotnetApp/SampleDotnetApp/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace SampleDotnetApp.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ SampleDotnetApp/SampleDotnetApp/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SampleDotnetApp.Startup))]
+namespace SampleDotnetApp
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
